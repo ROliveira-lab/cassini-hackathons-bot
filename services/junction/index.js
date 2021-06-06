480814db116d2bb82d7f1442f75ea15f1d1651a3
Benoit De Vrieze <benoit.devrieze@outlook.be>
2021-06-06 13:16:28 +0200

    Safe authentication for junction (retries + expiration)

--- services/junction/index.js
+++ services/junction/index.js
@@ -1,45 +1,72 @@
 const axios = require("axios");
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const moment = require("moment");
 
 const { Event, Participant } = require("./model");
-
 
 const junctionapi = axios.create({ baseURL: "https://app.hackjunction.com/api/" });
 
 // Authentication
 
-async function authenticate() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://app.hackjunction.com/login");
-  await page.waitForNavigation();
-  await page.waitForSelector(".auth0-lock-quiet");
-  await page.type("input[type=email]", process.env.JUNCTION_EMAIL);
-  await page.type("input[type=password]", process.env.JUNCTION_PASSWORD);
-  await page.click("button[type=submit]");
-  let callbackpage = await page.waitForNavigation();
-  let callbackparams = Object.fromEntries(new URLSearchParams(new URL(callbackpage.frame().url()).hash.substring(1)));
-  await browser.close();
-  return callbackparams.id_token;
+async function authenticate(attempts = 3) {
+  for (let i = 0; i < attempts; i++) {
+    let browser = await puppeteer.launch();
+    try {
+      let page = await browser.newPage();
+      await page.goto("https://app.hackjunction.com/login");
+      await page.waitForNavigation();
+      await page.waitForSelector(".auth0-lock-quiet");
+      await page.screenshot({ path: 'screenshots/junction.png' });
+      await page.type("input[type=email]", process.env.JUNCTION_EMAIL);
+      await page.type("input[type=password]", process.env.JUNCTION_PASSWORD);
+      await page.click("button[type=submit]");
+      let callbackpage = await page.waitForNavigation();
+      let callbackparams = Object.fromEntries(new URLSearchParams(new URL(callbackpage.frame().url()).hash.substring(1)));
+      let token = callbackparams.id_token;
+      let expires = moment().add(callbackparams.expires_in, 'seconds');
+      return { token, expires };
+    } catch (error) {
+      console.error(`Junction authentication failed: ${error.message}`);
+    } finally {
+      await browser.close();
+    }
+  }
+  console.error(`Junction authentication failed ${attempts} times`);
+  return null;
 }
 
-const authentication = authenticate().then((token) => { junctionapi.defaults.headers.common["Authorization"] = `Bearer ${token}`; });
+var authpromise = authenticate();
+
+async function ensureauthentication() {
+  let authresult = await authpromise
+  if (authresult && authresult.expires > moment()) {
+    junctionapi.defaults.headers.common["Authorization"] = `Bearer ${authresult.token}`;
+  } else {
+    authpromise = authenticate();
+    authresult = await authpromise;
+    if (authresult && authresult.expires > moment()) {
+      junctionapi.defaults.headers.common["Authorization"] = `Bearer ${authresult.token}`;
+    } else {
+      delete junctionapi.defaults.headers.common["Authorization"];
+    }
+  }
+}
 
 // Event
 
 var defaultevent = process.env.JUNCTION_DEFAULTEVENT;
 
 async function getevent(slug = defaultevent) {
-  await authentication;
-  let response = await junctionapi.get(`/events/${slug}`);
+  await ensureauthentication();
+  let response = await junctionapi.get(`/events/${slug}`).catch((error) => console.error(error.message));
   return response ? new Event(response.data) : undefined;
 }
 
 // Participants
 
 async function getparticipants(slug = defaultevent) {
-  await authentication;
-  let response = await junctionapi.get(`/registrations/${slug}/all`);
+  await ensureauthentication();
+  let response = await junctionapi.get(`/registrations/${slug}/all`).catch((error) => console.error(error.message));
   return response ? response.data.map(item => new Participant(item)) : undefined;
 }
 
